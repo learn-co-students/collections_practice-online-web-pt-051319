94dbcfb50b36c1d53de1198d5d9ca14b6345aa6b
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-05-18 00:22:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc (numbers)
+   numbers.sort
+end
+
+def sort_array_desc (numbers)
+  numbers.sort.reverse
+end
+
+def sort_array_char_count (array)
+  array.sort {|a,b|  a.length <=> b.length }
+end
+
+def swap_elements (array)
+  array [1..2] = array[1..2].reverse
+  array[0..2]
+end
+
+def reverse_array (array)
+  array.reverse {|element| element.sort }
+end
+
+def kesha_maker (array)
+ array =  array.each {|element|
+ element.slice!(2)
+ element.insert(2,"$")
+  }
+  array
+end
+
+def find_a(array)
+  array.select {|element|
+  element.start_with?("a")
+  }
+end
+
+def sum_array (array)
+  array.inject { |sum, n| sum + n }  
+end
+
+def add_s (array)
+  array.map {|word|
+    if word != "feet"
+      word << "s"
+    else
+      "feet"
+    end
+  }
+end
+  
 
