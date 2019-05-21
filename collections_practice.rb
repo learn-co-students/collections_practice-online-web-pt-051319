388611d5042cc56322bebc154ce04fe374b51939
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-21 01:35:24 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,61 @@
+require "pry"
+
+def sort_array_asc(array)
+  return array.sort
+end 
+
+def sort_array_desc(array)
+  return array.sort! {|x, y| y <=> x}
+end 
+
+def sort_array_char_count(array)
+  return array.sort {|x, y| x.length <=> y.length}
+end 
+
+def swap_elements(array)
+  return array.insert(2, array.delete_at(1))
+end 
+
+def reverse_array(array)
+  return array.reverse
+end 
+
+def kesha_maker(array)
+  newArray = []
+  array.each do |string|
+    stringA = string.split("")
+      stringA.insert(2, "$")
+      stringA.delete_at(3)
+     newArray << stringA.join("")
+    end 
+    return newArray
+end 
+
+def find_a(array)
+  array.select {|string| string.start_with?("a") }
+end 
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end 
+
+def add_s(array)
+    newArray = []
+  array.each do |string|
+    stringA = string.split("")
+      stringA.push("s")
+     newArray << stringA.join("")
+   end 
+   special = newArray[1].split("")
+   special.pop()
+   newArray.insert(1, special.join(""))
+   newArray.delete_at(2)
+   return newArray
+end 
+
+
+
+
+
+
 
